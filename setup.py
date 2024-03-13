b74c06065f1b669a6d74898b999b131afc5fe71b
Kedar Chandrayan <kedarchandrayan@gmail.com>
2024-03-13 19:50:49 +0530

    Version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='needlehaystack',
-    version='0.0.2',
+    version='0.1.0',
     author='Greg Kamradt',
     author_email='greg@gregkamradt.com',
     description='Doing simple retrieval from LLM models at various context lengths to measure accuracy.',
